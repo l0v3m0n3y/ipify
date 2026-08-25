c7a7e1402df5634f42e552bb27e2a612c96789c5
in my mind... <94906343+l0v3m0n3y@users.noreply.github.com>
2026-08-25 18:24:38 +0300

    Add HTTPMethod enum for request methods

--- src/ipify.swift
+++ src/ipify.swift
@@ -20,6 +20,14 @@ extension URLSession {
     }
 }
 
+public enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+    case patch = "PATCH"
+}
+
 public class Ipify {
     private let api = "ipify.org"
     private var headers: [String: String]
